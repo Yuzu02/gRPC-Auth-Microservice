27bc17d9e26ce24d16ef7cb1a1dbce24c197f4c5
Yuzu <Yuzu_0204@outlook.com>
2025-04-22 22:55:54 -0400

    refactor(Example): :bug: Improved and fixex roles management on the protected Controller

--- apps/api_gateway/src/protected.controller.ts
+++ apps/api_gateway/src/protected.controller.ts
@@ -1,26 +1,32 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { Roles } from './decorators/roles.decorator';
-
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('api')
+@UseGuards(JwtAuthGuard)
 export class ProtectedController {
-    @Get('user-info')
-    async getUserInfo(@Request() req) {
-        return {
-            userId: req.user.userId,
-            roles: req.user.roles,
-            permissions: req.user.permissions,
-        };
-    }
+  @Get('user-info')
+  async getUserInfo(@Request() req) {
+    return {
+      userId: req.user.userId,
+      roles: req.user.roles,
+      permissions: req.user.permissions,
+    };
+  }
 
-    @Get('admin-only')
-    @Roles('admin')
-    async getAdminData() {
-        return { message: 'Este es un endpoint solo para administradores' };
-    }
+  @Get('admin-only')
+  @Roles('admin')
+  async getAdminData() {
+    return { message: 'Este es un endpoint solo para administradores' };
+  }
 
-    @Get('public-data')
-    async getPublicData() {
-        return { message: 'Estos son datos disponibles para usuarios autenticados' };
-    }
+  @Get('public-data')
+  @Public()
+  async getPublicData() {
+    return {
+      message:
+        'Estos son datos disponibles para todos los usuarios, incluso sin autenticación',
+    };
+  }
 }
